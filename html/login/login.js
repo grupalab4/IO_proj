094ca5d8accf558b34e6ff6f8d3e2cf90787fc32
Łukasz Niedzielski <luk20001122@gmail.com>
2022-12-13 16:55:40 +0100

    handle submit login form

--- html/login/login.js
+++ html/login/login.js
@@ -1,3 +1,16 @@
 document.getElementById("login_form").onsubmit = (event) => {
-    console.log(event);
+    let formData = new FormData(event.target);
+    let login = formData.get("login");
+    let password = formData.get("password");
+    
+    let payload = {
+        login: login,
+        password: password,
+    };
+    
+    // console.log(JSON.stringify(payload));
+
+    // ten link się raczej na pewno zmieni
+    let result = postData("/ajax/login", payload);
+    result.finally(console.log);
 }
