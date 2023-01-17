e913155016c2e0515a2364649e4c70585b6e2728
Pawel Samula <=>
2023-01-17 16:02:09 +0100

    Added calorieIntake endpoint GET mapping

--- src/main/java/pl/io_proj/controller/CalculatorController.java
+++ src/main/java/pl/io_proj/controller/CalculatorController.java
@@ -2,6 +2,7 @@ package pl.io_proj.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.time.LocalDate;
 public class CalculatorController {
     private CalculatorService calculatorService;
 
+    @GetMapping
     public ResponseEntity<Integer> getDailyCalorieIntake(@RequestParam double goalWeight, @RequestParam LocalDate deadline) {
         Integer dailyCalorieIntake = this.calculatorService.getDailyCalorieIntake(goalWeight, deadline);
         return new ResponseEntity<>(dailyCalorieIntake, HttpStatus.OK);
