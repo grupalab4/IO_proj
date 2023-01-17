a1e30cc741edb4fbfd78b7f7edbc5ebd3b02eca7
Mriabcew <92296396+Mriabcew@users.noreply.github.com>
2023-01-17 18:18:28 +0100

    Finding and returning product info (#10)

--- src/main/java/pl/io_proj/controller/ProductController.java
+++ src/main/java/pl/io_proj/controller/ProductController.java
@@ -2,7 +2,10 @@ package pl.io_proj.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pl.io_proj.model.Product;
 import pl.io_proj.service.ProductService;
 
 @RestController
@@ -19,5 +22,19 @@ public class ProductController {
     @GetMapping("/test")
     public String test(HttpServletRequest request) {
         return service.test(request);
+    }
+
+    @GetMapping("/get-product-name")
+    public ResponseEntity<?> getProductName(@RequestParam String productName) {
+
+        try {
+            Product product = service.getProductByName(productName);
+
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+
+
     }
 }
--- src/main/java/pl/io_proj/service/ProductService.java
+++ src/main/java/pl/io_proj/service/ProductService.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import pl.io_proj.model.Product;
 import pl.io_proj.repository.ProductRepository;
 
 @Service
@@ -17,6 +18,15 @@ public class ProductService {
 
     public String test(HttpServletRequest request) {
         return request.toString();
+    }
+
+    public Product getProductByName(String name) throws Exception {
+
+        if (name == null || name.isEmpty())
+            throw new Exception("Product name is empty");
+
+        return repository.findByName(name)
+                .orElseThrow(() -> new Exception("Product not found"));
     }
 }
 
