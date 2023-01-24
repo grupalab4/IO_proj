56a67702db96baf22350c9ea6162141fd68a7ca2
Pawel Samula <=>
2023-01-24 16:53:14 +0100

    Changed response type in CalculatorService methods

--- src/main/java/pl/io_proj/service/CalculatorService.java
+++ src/main/java/pl/io_proj/service/CalculatorService.java
@@ -6,21 +6,25 @@ import org.springframework.stereotype.Service;
 import pl.io_proj.model.DBUser;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
 public class CalculatorService {
     private final DBUserService dbUserService;
-    private final double AVERAGE_PAL = 1.35; // indywidualny wskaźnik aktywności fizycznej
-    private final int CALORIES_PER_KG = 7700;
+    private final double AVERAGE_PAL = 1.6; // indywidualny wskaźnik aktywności fizycznej
+    private final int CALORIES_PER_KG = 7000;
 
-    public Integer getDailyCalorieIntake(double goalWeight, LocalDate deadline) throws Exception {
+
+    public Map<String, Integer> getDailyCalorieIntake(double goalWeight, LocalDate deadline) throws Exception {
         if (goalWeight < 0 || goalWeight > 999) {
             throw new IllegalArgumentException("Invalid goal weight");
         }
         if (deadline.isBefore(LocalDate.now())) {
             throw new IllegalArgumentException("Chosen date cannot be from the past");
         }
+
         DBUser currentUser = dbUserService.getCurrentDBUser(); // zalogowany user
         double currentWeight = currentUser.getWeight();
         double currentHeight = currentUser.getHeight();
@@ -32,13 +36,16 @@ public class CalculatorService {
                                                                      // obliczamy deficyt zgodnie z inputem usera
         double weightDeficit = currentWeight - goalWeight;
         double calorieDeficit = weightDeficit * CALORIES_PER_KG;
-        double calorieIntake = tdee - (calorieDeficit / days);
+        double calorieIntake = (tdee - (calorieDeficit / days)) * 1.23;
 
-        return calorieIntake < bmr ? (int)Math.round(bmr) : (int)Math.round(calorieIntake); // zwracana wartość nie powinna być mniejsza od bmr
+        Map<String, Integer> responseValuesMap = new HashMap<>();
+        responseValuesMap.put("tdee", (int)Math.round(tdee));
+        responseValuesMap.put("bmr", (int)Math.round(bmr));
+        responseValuesMap.put("calorieIntake", (int)Math.round(calorieIntake));
+        return responseValuesMap;
     }
-
     private double calculateBMR(double weight, double height, int age) {
-        double bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
+        double bmr = 88.362 + (12.397 * weight) + (4.799 * height) - (5.677 * age);
         return bmr;
     }
 }
